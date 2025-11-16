0ac4b302ead6e58bd61d9330224cee1b9e3f9a0f
Jimmy Ding <jimmyguding@gmail.com>
2025-11-16 12:00:20 -0600

    feat: added tools to mise

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ terraform {
   required_version = ">=1.10.0"
 
   required_providers {
-    http = {
-      source  = "hashicorp/http"
-      version = ">= 3.5.0"
-    }
-
     local = {
       source  = "hashicorp/local"
       version = ">= 2.5.3"
@@ -24,11 +19,6 @@ terraform {
   }
 }
 
-data "http" "personal_ipv4" {
-  count = 1
-  url   = "https://ipv4.icanhazip.com"
-}
-
 module "talos" {
   source  = "hcloud-talos/talos/hcloud"
   version = "v2.20.2"
@@ -38,18 +28,19 @@ module "talos" {
   cluster_prefix            = true
   datacenter_name           = "ash-dc1"
   firewall_use_current_ip   = false
-  firewall_kube_api_source  = ["${chomp(data.http.personal_ipv4[0].response_body)}/32"]
-  firewall_talos_api_source = ["${chomp(data.http.personal_ipv4[0].response_body)}/32"]
+  firewall_kube_api_source  = ["0.0.0.0/0"]
+  firewall_talos_api_source = ["0.0.0.0/0"]
   talos_version             = "v1.11.5"
   control_plane_count       = 3
-  control_plane_server_type = "cpx11"
+  control_plane_server_type = "cpx21"
   worker_nodes = [
     {
       type = "cpx51"
     }
   ]
   # arm does not exist on US datacenters yet
-  disable_arm = true
+  disable_arm        = true
+  kubernetes_version = "1.34.2"
 }
 
 resource "local_file" "talosconfig" {
